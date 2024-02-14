767faecd3e17ffd82898575edf41e0079ceae0d5
seafom <sibelle.bergier@artfx.fr>
2024-02-14 22:11:34 +0100

    Pokemon finish

---
+++ PokemonArena/Main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+using namespace std;
+
+int Hurt(int PokemonLife, int Aattack) {
+    if (Aattack == 0) {
+        PokemonLife -= 20;
+    }
+    else if (Aattack == 1) {
+        PokemonLife -= 14;
+    }
+    else if (Aattack == 2) {
+        PokemonLife -= 36;
+    }
+    else if (Aattack == 3) {
+        PokemonLife -= 10;
+    }
+    return PokemonLife;
+}
+
+int Damage(int PokemonLife, int Pattack) {
+    if (Pattack == 0) {
+        PokemonLife -= 20;
+    }
+    else if (Pattack == 1) {
+        PokemonLife -= 14;
+    }
+    else if (Pattack == 2) {
+        PokemonLife -= 36;
+    }
+    else if (Pattack == 3) {
+        PokemonLife -= 10;
+    }
+    return PokemonLife;
+}
+
+int main()
+{
+    // Random Number Generator
+    srand(static_cast<unsigned int>(time(0)));
+    int SnorlaxLife = 120; 
+    int CharmanderLife = 92;  
+    string trainer;
+
+    cout << "==============================\n" << endl;
+    cout << "Welcome to Pokemon Battle\n" << endl;
+    cout << "==============================\n" << endl;
+
+    cout << "What's your name ?\n" << endl;
+    cin >> trainer;
+    cout << "Welcome, " << trainer << "!" << endl;
+    cout << "A wild Pokemon appears!" << endl;
+    cout << endl;
+
+    cout << "Snorlax, I choose you!" << endl;  
+
+    do {
+        cout << "Choose an attack:" << endl;
+        cout << "0. Charge\n1. High Ground\n2. Mega Smash\n3. Hate" << endl;
+        int playerAttack;
+        cin >> playerAttack;
+
+        CharmanderLife = Damage(CharmanderLife, playerAttack);  
+        cout << "Charmander's remaining life: " << CharmanderLife << endl;
+
+        if (CharmanderLife <= 0) {
+            cout << "Charmander fainted! " << trainer << " wins, Congratulations !" << endl;
+            break;
+        }
+
+        cout << "Charmander uses a move!" << endl;
+        int enemyAttack = rand() % 4;
+        SnorlaxLife = Hurt(SnorlaxLife, enemyAttack);  
+        cout << "Snorlax's remaining life: " << SnorlaxLife << endl;
+
+        if (SnorlaxLife <= 0) {
+            cout << "Snorlax fainted! " << trainer << " loses, that's a shame..." << endl;
+            break;
+        }
+
+    } while (CharmanderLife > 0 && SnorlaxLife > 0);
+
+    return 0;
+}
